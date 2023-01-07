716077cd6b012e8e687de4b43173cfe6d188678e
Aldy Syachranie <aldysyach17@gmail.com>
2023-01-07 20:28:35 +0700

    awikwok 2

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -50,14 +50,11 @@ export default defineComponent({
         IonToolbar,
     },
     setup() {
-        const get = async () => {
-            const res = await axios.get(
-                "https://jsonplaceholder.typicode.com/todos/1"
-            );
-            console.log(res);
-        };
-
-        get();
+        axios
+            .get("https://jsonplaceholder.typicode.com/todos/1")
+            .then((res) => {
+                console.log(res);
+            });
     },
 });
 </script>
